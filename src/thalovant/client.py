8b4bf84eb3412c06c0921fb1dba4fde87a85367c
Gaëtan Trellu <gaetan.trellu@gmail.com>
2026-06-09 09:12:51 -0400

    Release Python SDK 0.4.11

--- src/thalovant/client.py
+++ src/thalovant/client.py
@@ -49,7 +49,7 @@ from .transport import HiveMindHTTPTransport, HiveMindMQTTTransport, HiveMindWSS
 from .protocols import HubProtocol
 
 
-DEFAULT_USERAGENT = "ThalovantPythonSDK/0.4.10"
+DEFAULT_USERAGENT = "ThalovantPythonSDK/0.4.11"
 
 
 def _transport_for_protocol(
